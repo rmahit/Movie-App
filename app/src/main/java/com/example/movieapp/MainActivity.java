b0d2fbc58f52d08bb0e67fbecf92cf78376f4bce
rmahit <mahitrbhanu@gmail.com>
2024-12-17 11:39:39 +0530

    project created

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.movieapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText e1,e2;
+    Button b1;
+    String getU,getP;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        e1=(EditText) findViewById(R.id.uname);
+        e2=(EditText) findViewById(R.id.pass);
+        b1=(Button) findViewById(R.id.logbut);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+             getU=e1.getText().toString();
+             getP=e2.getText().toString();
+
+            }
+        });
+
 
     }
 }
